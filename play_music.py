c5a993fb69ea48c6daa949e57bf6d133e4e9f88f
m-k-q <m.k.quast@@gmail.com>
2020-08-06 14:06:36 +0200

    new function: change_song_and_get_new_index

--- play_music.py
+++ play_music.py
@@ -5,9 +5,19 @@ import RPi.GPIO as GPIO
 import time
 
 
-PINS = [25, 24, 23]
-for pin in PINS:
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+class Button:
+    def __init__(self, pin, state):
+        self.pin = pin
+        self.state = state
+    
+previous_button = Button(25, False)
+play_button = Button(24, False)
+next_button = Button(23, False)
+
+buttons = [previous_button, play_button, next_button]
+
+for button in buttons:
+    GPIO.setup(button.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 mp3_files = [ f for f in listdir('songs') if f[-4:] == '.mp3' ]
 
@@ -24,21 +34,38 @@ a_song_is_currently_playing = False
 song_is_paused = False
 
 pygame.init()
-buttons_state = [False, False, False]
+
+
+def change_song_and_get_new_index(direction, songs, index):
+    if direction=='previous':
+        index -= 1
+        if index < 0:
+            index = len(songs)-1
+    elif direction=='next':
+        index += 1
+        if index >= len(songs):
+            index = 0
+    elif direction=='random':
+        if len(songs)!=1:
+            new_index = -1
+            while new_index != index:
+               new_index = random.randint(0, len(songs)-1)
+            index = new_index
+    
+    print("--- " + mp3_files[index] + " ---")        
+    pygame.mixer.music.load('songs/' + mp3_files[index])
+    return index
+
 
 while True:
     a_song_is_currently_playing = pygame.mixer.music.get_busy()
-    buttons_state = list(not GPIO.input(PINS[button]) for button in range(3))
-    
+    for button in buttons:
+        button.state = not GPIO.input(button.pin)    
         
-    if buttons_state[0]:
-        index -= 1
-        if index < 0:
-            index = len(mp3_files)-1
-        print("--- " + mp3_files[index] + " ---")
-        pygame.mixer.music.load('songs/' + mp3_files[index])
+    if previous_button.state:
+        index = change_song_and_get_new_index('previous', mp3_files, index)
 
-    if buttons_state[1]:
+    if play_button.state:
         if song_is_paused:
             pygame.mixer.music.unpause()
             song_is_paused = False
@@ -48,13 +75,9 @@ while True:
     elif not song_is_paused:
         pygame.mixer.music.pause()
         song_is_paused = True
-        
-    if buttons_state[2]:
-        index += 1
-        if index >= len(mp3_files):
-            index = 0
-        print("--- " + mp3_files[index] + " ---")
-        pygame.mixer.music.load('songs/' + mp3_files[index])
 
-        
+
+    if next_button.state:
+        index = change_song_and_get_new_index('next', mp3_files, index)
+
     time.sleep(0.25)
